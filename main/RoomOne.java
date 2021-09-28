f3435296ed94ac4b1518325bf2ae1567c1cacb7d
Yasir Dahir <60136627+ydairz@users.noreply.github.com>
2021-09-28 23:05:24 +0100

    Add files via upload

--- main/RoomOne.java
+++ main/RoomOne.java
@@ -10,10 +10,17 @@ import javax.imageio.ImageIO;
 public class RoomOne extends GameState {
 
     private Player player;
+    private Wall[] wall = new Wall[3];
 
     public RoomOne(GameStateManager gsm) {
         super(gsm);
-        this.player = new Player(0, 0, 25, 25);
+        this.player = new Player(25, 25, 25, 25);
+        // Generate walls.
+        for (int i = 0; i < wall.length-1; i = i + 3) {
+            wall[i] = new Wall(0+(i*50), 75, 250, 50);
+            wall[i+1] = new Wall(200+(i*50), 125, 50, 175);
+            wall[i+2] = new Wall(325+(i*50), 0, 50, 300);
+        }
     }
 
     @Override
@@ -26,6 +33,10 @@ public class RoomOne extends GameState {
         // Draws a custom background image of the level (in the resources folder).
         BufferedImage bufferedImage = getBackgroundImage();
         g.drawImage(bufferedImage, 0, 0, null);
+        // Draws the walls.
+        for (int i = 0; i < this.wall.length; i++) {
+            this.wall[i].draw(g);
+        }
         // Update the player object's position.
         this.player.draw(g);
     }
---
+++ main/Wall.java
@@ -0,0 +1,31 @@
+package main;
+import java.awt.Rectangle;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Wall {
+    
+    // The position of the wall relative to the GamePanel.
+    private int x;
+    private int y;
+    // Wall's hitbox variables - hitbox is used for collision detection.
+    private Rectangle hitbox;
+    private int width;
+    private int height;
+    
+    public Wall(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        // The dimensions of the player hitbox.
+        this.width = width;
+        this.height = height;
+        this.hitbox = new Rectangle(this.x, this.y, this.width, this.height);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(this.x, this.y, this.width, this.height);
+        g.setColor(Color.BLUE);
+        g.drawRect(this.x, this.y, this.width, this.height);
+    }
+}
