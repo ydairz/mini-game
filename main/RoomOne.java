27c83c02e6d1343ceb305f2868347e45cf9a32f3
Yasir Dahir <60136627+ydairz@users.noreply.github.com>
2021-09-28 23:33:55 +0100

    Add files via upload

--- main/RoomOne.java
+++ main/RoomOne.java
@@ -4,23 +4,27 @@ import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.File;
-
 import javax.imageio.ImageIO;
 
 public class RoomOne extends GameState {
 
     private Player player;
-    private Wall[] wall = new Wall[3];
+    private Wall[] wall = new Wall[10];
 
     public RoomOne(GameStateManager gsm) {
         super(gsm);
         this.player = new Player(25, 25, 25, 25);
         // Generate walls.
-        for (int i = 0; i < wall.length-1; i = i + 3) {
-            wall[i] = new Wall(0+(i*50), 75, 250, 50);
-            wall[i+1] = new Wall(200+(i*50), 125, 50, 175);
-            wall[i+2] = new Wall(325+(i*50), 0, 50, 300);
-        }
+        wall[0] = new Wall(0, 75, 250, 50);
+        wall[1] = new Wall(200, 125, 50, 175);
+        wall[2] = new Wall(325, 0, 50, 300);
+        wall[3] = new Wall(0, 375, 700, 50);
+        wall[4] = new Wall(75, 200, 50, 100);
+        wall[5] = new Wall(450, 75, 250, 50);
+        wall[6] = new Wall(450, 125, 50, 250);
+        wall[7] = new Wall(575, 200, 225, 50);
+        wall[8] = new Wall(650, 512, 50, 50);
+        wall[9] = new Wall(525, 425, 50, 50);
     }
 
     @Override
